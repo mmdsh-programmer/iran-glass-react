087c326f8e1d07a990a8a55afe41fff12b8dd5ed
mmdsh-programmer <programmer.mmdsh@gmail.com>
2021-11-17 14:06:34 +0330

    add framer motion to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,30 @@
 import "styles/App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Switch, Route, useLocation } from "react-router-dom";
 import { Header } from "./Header";
+import { Footer } from "./Footer";
 import { Home } from "pages/Home";
 import { NotFound } from "pages/NotFound";
 import { CategoryDetails } from "pages/CategoryDetails";
 import { SmoothScroll } from "./SmoothScroll";
+import { AnimatePresence } from "framer-motion";
 
 export default function App() {
+  const location = useLocation();
   return (
-    <Router>
-      <SmoothScroll>
-        <Header />
-        <Switch>
+    <SmoothScroll>
+      <Header />
+      <AnimatePresence exitBeforeEnter initial={true}>
+        <Switch location={location} key={location.pathname}>
           <Route exact path="/" component={Home} />
           <Route exact path="/category-details" component={CategoryDetails} />
           <Route component={NotFound} />
         </Switch>
-      </SmoothScroll>
-    </Router>
+      </AnimatePresence>
+      {location.pathname === "/" ? (
+        <Footer classes="home-footer" />
+      ) : (
+        <Footer />
+      )}
+    </SmoothScroll>
   );
 }
