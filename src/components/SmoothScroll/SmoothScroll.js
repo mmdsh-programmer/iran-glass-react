537cc1cd0b210f1df5e63c9d7c27fbceacb0e175
mmdsh-programmer <programmer.mmdsh@gmail.com>
2021-11-17 10:45:50 +0330

    fix smooth scroll completely

--- src/components/SmoothScroll/SmoothScroll.js
+++ src/components/SmoothScroll/SmoothScroll.js
@@ -1,28 +1,10 @@
-// import Scrollbar from "smooth-scrollbar";
-// import { useEffect } from "react";
-// import OverscrollPlugin from "smooth-scrollbar/plugins/overscroll";
-
-// export default function SmoothScroll() {
-//   useEffect(() => {
-//     Scrollbar.use(OverscrollPlugin);
-
-//     Scrollbar.init(document.body, {
-//       damping: 0.03,
-//       plugins: {
-//         overscroll: false,
-//       },
-//     });
-
-//     return () => {
-//       if (Scrollbar) Scrollbar.destroy(document.body);
-//     };
-//   }, []);
-
-//   return null;
-// }
-
 import { useEffect, useRef, useState } from "react";
-import { gsap, Power2 } from "gsap";
+import {
+  useViewportScroll,
+  useTransform,
+  useSpring,
+  motion,
+} from "framer-motion";
 import styles from "./SmoothScroll.module.css";
 
 export default function SmoothScroll({ children }) {
@@ -36,29 +18,26 @@ export default function SmoothScroll({ children }) {
     }
   });
 
-  const onScroll = () => {
-    gsap.to(viewportRef.current, {
-      force3D: false,
-      rotate: 0,
-      y: -window.pageYOffset,
-      duration: 3,
-      ease: Power2.easeOut,
-    });
-  };
-
   useEffect(() => {
-    window.addEventListener("scroll", onScroll);
     ro.observe(viewportRef.current);
-  }, []);
+  });
+
+  const { scrollY } = useViewportScroll(); // measures how many pixels user has scrolled vertically
+  // as scrollY changes between 0px and the scrollable height, create a negative scroll value...
+  // ... based on current scroll position to translateY the document in a natural way
+  const transform = useTransform(scrollY, [0, height], [0, -height]);
+  const physics = { damping: 20, mass: 1, stiffness: 55 };
+  const spring = useSpring(transform, physics);
 
   return (
     <>
-      <div
+      <motion.div
         className={`${styles["viewport"]} scroll-container`}
         ref={viewportRef}
+        style={{ y: spring }}
       >
         {children}
-      </div>
+      </motion.div>
       <div
         style={{
           height: height,
@@ -67,61 +46,3 @@ export default function SmoothScroll({ children }) {
     </>
   );
 }
-
-// import { useEffect, useRef } from "react";
-// import { useHistory } from "react-router";
-// import useWindowSize from "hooks/useWindowSize";
-// import styles from "./SmoothScroll.module.css";
-
-// export default function SmoothScroll({ children }) {
-//   const app = useRef(null);
-//   const scrollContainer = useRef(null);
-//   const history = useHistory();
-//   console.log(history);
-//   const { width, height } = useWindowSize();
-//   const scrollConifg = {
-//     ease: 0.08,
-//     current: 0,
-//     previous: 0,
-//     rounded: 0,
-//   };
-
-//   useEffect(() => {
-//     setBodyHeight();
-//     requestAnimationFrame(() => scrolling());
-//   }, []);
-
-//   useEffect(() => {
-//     setBodyHeight();
-//   }, [height, width]);
-
-//   const scrolling = () => {
-//     scrollConifg.current = window.scrollY;
-//     // Set Previous to the scroll previous position
-//     scrollConifg.previous +=
-//       (scrollConifg.current - scrollConifg.previous) * scrollConifg.ease;
-//     // Set rounded to
-//     scrollConifg.rounded = Math.abs(scrollConifg.previous * 100) / 100;
-
-//     //Assign smooth scrolling to the scroll container
-//     scrollContainer.current.style.transform = `translateY(-${scrollConifg.rounded}px)`;
-
-//     requestAnimationFrame(() => scrolling());
-//   };
-
-//   const setBodyHeight = () => {
-//     document.body.style.height = `${
-//       scrollContainer.current.getBoundingClientRect().height
-//     }px`;
-//   };
-
-//   return (
-//     <>
-//       <div ref={app} className={styles["app"]}>
-//         <div ref={scrollContainer} className={styles["scroll"]}>
-//           {children}
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
