a4fd5bcfbf82c952144711fa7226517e88b7e399
mmdsh-programmer <programmer.mmdsh@gmail.com>
2021-11-18 11:42:16 +0330

    add preloader to page

--- src/components/App.js
+++ src/components/App.js
@@ -8,33 +8,55 @@ import { CategoryDetails } from "pages/CategoryDetails";
 import { SmoothScroll } from "./SmoothScroll";
 import { AnimatePresence } from "framer-motion";
 import { Preloader } from "./Preloader";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [isLoading, setLoading] = useState(true);
   const location = useLocation();
 
-  useEffect(() => {
+  const calcLatency = () => {
     let startTime = new Date().getTime();
     let latency = startTime - performance.timing.navigationStart;
+    return latency;
+  };
+
+  const fakeRequest = (timeout) => {
+    return new Promise((resolve) =>
+      setTimeout(() => resolve(timeout), (timeout + 1000) * 2 + 600)
+    );
+  };
+
+  useEffect(() => {
+    fakeRequest(calcLatency()).then(() => {
+      setLoading(false);
+    });
   });
+
   return (
     <>
-      {/* <Preloader /> */}
-      <SmoothScroll>
-        <Header />
-        <AnimatePresence exitBeforeEnter initial={true}>
-          <Switch location={location} key={location.pathname}>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/category-details" component={CategoryDetails} />
-            <Route component={NotFound} />
-          </Switch>
-        </AnimatePresence>
-        {location.pathname === "/" ? (
-          <Footer classes="home-footer" />
-        ) : (
-          <Footer />
-        )}
-      </SmoothScroll>
+      {isLoading ? (
+        <Preloader latency={calcLatency()} />
+      ) : (
+        <SmoothScroll>
+          <Header />
+          <AnimatePresence exitBeforeEnter initial={true}>
+            <Switch location={location} key={location.pathname}>
+              <Route exact path="/" component={Home} />
+              <Route
+                exact
+                path="/category-details"
+                component={CategoryDetails}
+              />
+              <Route component={NotFound} />
+            </Switch>
+          </AnimatePresence>
+          {location.pathname === "/" ? (
+            <Footer classes="home-footer" />
+          ) : (
+            <Footer />
+          )}
+        </SmoothScroll>
+      )}
     </>
   );
 }
--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -1,18 +1,43 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState, useMemo } from "react";
+import { useHistory } from "react-router";
 import styles from "./Preloader.module.css";
-import gsap from "gsap";
+import { gsap, Power2 } from "gsap";
 
 export default function Preloader(props) {
+  const [percent, setPercent] = useState(0);
   const indicatorRef = useRef(null);
-  const [percent, setPercent] = useState(50);
+  const progressRef = useRef(null);
+  const preloaderContainerRef = useRef(null);
+  const { latency } = props;
+  const loadProgress = gsap.timeline();
 
-  useEffect(() => {});
+  useEffect(() => {
+    loadProgress.to(indicatorRef.current, {
+      yPercent: -100,
+      duration: latency / 1000 + 1,
+      ease: Power2.easeOut,
+      onUpdate: () => {
+        progressRef.current.innerText = `${
+          (loadProgress.progress() * 100) >> 0
+        } %`;
+      },
+      onComplete: () => {
+        gsap.to(preloaderContainerRef.current, {
+          opacity: 0,
+          duration: 0.5,
+          delay: latency / 1000 + 1,
+          ease: Power2.easeOut,
+        });
+      },
+    });
+  });
 
   return (
     <div
       className={`${styles["preloader-container"]} row justify-center align-center`}
+      ref={preloaderContainerRef}
     >
-      <span className={styles["preloader-percent"]}>{percent} %</span>
+      <span className={styles["preloader-percent"]} ref={progressRef}></span>
       <div className={styles["preloader-indicator"]} ref={indicatorRef}></div>
     </div>
   );
