536c838c8fe1bd6d5984f8bde2667b662cc1c72e
mmdsh-programmer <programmer.mmdsh@gmail.com>
2021-12-01 15:35:39 +0330

    add preloader component

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -1,10 +1,54 @@
-import React, { useEffect, useRef, useState, useMemo } from "react";
-import { useHistory } from "react-router";
+import React, { useEffect } from "react";
 import styles from "./Preloader.module.css";
 import { gsap, Power2 } from "gsap";
 
 export default function Preloader(props) {
-  useEffect(() => {}, []);
+  useEffect(() => {
+    let path = document.querySelector(".path");
+    const tween = gsap.timeline();
+    tween
+      .to(path, {
+        attr: { d: "M 0 100 V 50 Q 50 0 100 50 V 100 z" },
+        ease: Power2.easeIn,
+        duration: 1,
+      })
+      .to(path, {
+        attr: { d: "M 0 100 V 0 Q 50 0 100 0 V 100 z" },
+        ease: Power2.easeOut,
+        duration: 1.5,
+      })
+      .from(
+        ".content",
+        {
+          y: 200,
+          autoAlpha: 0,
+          duration: 1.5,
+          ease: Power2.easeOut,
+          onComplete: () => tween.reverse(),
+        },
+        "-=1.5"
+      );
+  }, []);
 
-  return <div></div>;
+  return (
+    <section className={styles["preloader-container"]}>
+      <svg
+        className={styles["transition"]}
+        viewBox="0 0 100 100"
+        preserveAspectRatio="none"
+      >
+        <path
+          className="path"
+          dur="10s"
+          fill="#424242"
+          vectorEffect="non-scaling-stroke"
+          d="M 0 100 V 100 Q 50 100 100 100 V 100 z"
+        />
+      </svg>
+
+      <div className={`${styles["container"]} ${styles["content"]} content`}>
+        <h1 className={styles["logo"]}>IGT</h1>
+      </div>
+    </section>
+  );
 }
