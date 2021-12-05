85fdd142beefa31e39c3da38126657016902477a
mmdsh-programmer <programmer.mmdsh@gmail.com>
2021-12-05 08:33:27 +0330

    change some code in index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from "react";
+import "styles/index.css";
 import ReactDOM from "react-dom";
 import App from "components/App";
 import { BrowserRouter as Router } from "react-router-dom";
+import("fonts/NittiGrotesk-Light.ttf");
+import("fonts/NittiGrotesk-Light.woff2");
+import("fonts/NittiGrotesk-Medium.ttf");
+import("fonts/NittiGrotesk-Medium.woff2");
+import("fonts/SaolDisplay-Light.ttf");
+import("fonts/SaolDisplay-Light.woff2");
+import("fonts/Shabnam.ttf");
 
 ReactDOM.render(
   <Router>
