788686d4ce889588a47cd5115d799f049159d3d2
mmdsh-programmer <programmer.mmdsh@gmail.com>
2021-12-05 09:42:14 +0330

    remove importing fonts

--- src/index.js
+++ src/index.js
@@ -3,13 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "components/App";
 import { BrowserRouter as Router } from "react-router-dom";
-import(/* webpackPreload: true */ "fonts/NittiGrotesk-Light.ttf");
-import(/* webpackPreload: true */ "fonts/NittiGrotesk-Light.woff2");
-import(/* webpackPreload: true */ "fonts/NittiGrotesk-Medium.ttf");
-import(/* webpackPreload: true */ "fonts/NittiGrotesk-Medium.woff2");
-import(/* webpackPreload: true */ "fonts/SaolDisplay-Light.ttf");
-import(/* webpackPreload: true */ "fonts/SaolDisplay-Light.woff2");
-import(/* webpackPreload: true */ "fonts/Shabnam.ttf");
 
 ReactDOM.render(
   <Router>
