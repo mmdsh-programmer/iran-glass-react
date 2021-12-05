c1d2207d22ff647a36e6a178016660669fac73bd
mmdsh-programmer <programmer.mmdsh@gmail.com>
2021-12-05 09:28:44 +0330

    add index.css to app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import "styles/index.css";
 import ReactDOM from "react-dom";
+import "./index.css";
 import App from "components/App";
 import { BrowserRouter as Router } from "react-router-dom";
 import(/* webpackPreload: true */ "fonts/NittiGrotesk-Light.ttf");
